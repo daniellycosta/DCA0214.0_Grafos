06678b0932d4056cbb3708cb18a943b99542909e
Danielly Costa <daniellycmcosta@gmail.com>
2018-12-11 13:39:07 -0300

    add método de ciclos simples

--- src/Graph.java
+++ src/Graph.java
@@ -1,3 +1,4 @@
+
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.Arrays;
@@ -8,202 +9,203 @@ import java.io.IOException;
 import java.awt.Color;
 
 public class Graph {
-	public int nVertices;
-	public int nEdges;
-	private ArrayList<LinkedList<Edge>> adjacentList;
-	private int[][] cordinades;
-	private GraphicsUtils graphics;
 
-	public void addEdge(int u, int v, double weight){
-		/*Dani*/
-		adjacentList.get(u).add(new Edge(u,v,weight));
-		adjacentList.get(v).add(new Edge(v,u,weight));
-		
-		nEdges=nEdges+1;
-	}
+    public int nVertices;
+    public int nEdges;
+    private ArrayList<LinkedList<Edge>> adjacentList;
+    private int[][] cordinades;
+    private GraphicsUtils graphics;
 
-	public void removeEdge(Edge edge){
-		/*Dani*/
-		adjacentList.get(edge.getU()).remove(new Edge(edge.getU(),edge.getV(),edge.getWeight()));
-		adjacentList.get(edge.getV()).remove(new Edge(edge.getV(),edge.getU(),edge.getWeight()));
-		nEdges=nEdges-1;
-	}
+    public void addEdge(int u, int v, double weight) {
+      
+        adjacentList.get(u).add(new Edge(u, v, weight));
+        adjacentList.get(v).add(new Edge(v, u, weight));
 
-	public LinkedList<Edge> getNeighbours(int index){
-		/*Dani*/
-		return adjacentList.get(index); //ver se é só isso mesmo
-	}
+        nEdges = nEdges + 1;
+    }
 
-	public boolean isEulerian(){
-		/*Dani*/
-		for(int i=0; i< adjacentList.size();i++) {
-			if(adjacentList.get(i).size()%2 != 0) {
-				return false;
-			}
-		}
+    public void removeEdge(Edge edge) {
+      
+        adjacentList.get(edge.getU()).remove(new Edge(edge.getU(), edge.getV(), edge.getWeight()));
+        adjacentList.get(edge.getV()).remove(new Edge(edge.getV(), edge.getU(), edge.getWeight()));
+        nEdges = nEdges - 1;
+    }
+
+    public LinkedList<Edge> getNeighbours(int index) {
+        
+        return adjacentList.get(index);
+    }
+
+    public boolean isEulerian() {
+     
+        for (int i = 0; i < adjacentList.size(); i++) {
+            if (adjacentList.get(i).size() % 2 != 0) {
+                return false;
+            }
+        }
         return true;
-	}
+    }
 
-	/***************************************************************************************/
-
-    
-    public boolean existEdges(){
-        if (nEdges==0) {
+    /**
+     * ************************************************************************************
+     */
+    public boolean existEdges() {
+        if (nEdges == 0) {
             return false;
-        }else{
+        } else {
             return true;
         }
     }
-    
-	public Graph(GraphicsUtils otherGraphics, final String mapFileName, String title){
-		this.graphics = new GraphicsUtils(otherGraphics, mapFileName, title);
-	}
 
-	public Graph(final String filePath, final String mapFileName, String title){
-		this.graphics = new GraphicsUtils(mapFileName, title);
-		BufferedReader br = null;
-		String line = "";
-		String cvsSplitBy = ";";		
- 		
-		try {	 	 
-			br = new BufferedReader(new FileReader(filePath));
-			this.nVertices = Integer.parseInt(br.readLine());
-			cordinades = new int[nVertices][2];
+    public Graph(GraphicsUtils otherGraphics, final String mapFileName, String title) {
+        this.graphics = new GraphicsUtils(otherGraphics, mapFileName, title);
+    }
 
-			adjacentList = new ArrayList<LinkedList<Edge>>();
-			for(int i=0; i<nVertices; i++){
-				adjacentList.add(new LinkedList<Edge>());
-			}
+    public Graph(final String filePath, final String mapFileName, String title) {
+        this.graphics = new GraphicsUtils(mapFileName, title);
+        BufferedReader br = null;
+        String line = "";
+        String cvsSplitBy = ";";
 
-			while ((line = br.readLine()) != null) {	
+        try {
+            br = new BufferedReader(new FileReader(filePath));
+            this.nVertices = Integer.parseInt(br.readLine());
+            cordinades = new int[nVertices][2];
 
-				String[] edges;
-				String[] e;			
-				String[] read = line.split(cvsSplitBy);
-				
-				cordinades[Integer.parseInt(read[0])][0] = Integer.parseInt(read[1]);
-				cordinades[Integer.parseInt(read[0])][1] = Integer.parseInt(read[2]);
+            adjacentList = new ArrayList<LinkedList<Edge>>();
+            for (int i = 0; i < nVertices; i++) {
+                adjacentList.add(new LinkedList<Edge>());
+            }
 
-				if(read.length==3){
-					break;
-				}	
+            while ((line = br.readLine()) != null) {
 
-				edges = read[3].split("%");
-				for (String edge : edges){
-					e = edge.split(":");
-					addEdge(Integer.parseInt(read[0]), Integer.parseInt(e[0]),
-								 Double.parseDouble(e[1]));
-				}
-			}	 
-		} catch (FileNotFoundException ex) {
-			ex.printStackTrace();
-		} catch (IOException ex) {
-			ex.printStackTrace();
-		} finally {
-			if (br != null) {
-				try {
-					br.close();
-				} catch (IOException ex) {
-					ex.printStackTrace();
-				}
-			}
-		}
-		for(int i=0; i<nVertices; i++){
-			graphics.addNode(cordinades[i][0],cordinades[i][1]);
-		}
+                String[] edges;
+                String[] e;
+                String[] read = line.split(cvsSplitBy);
 
-		for(LinkedList<Edge> verticeNeightbors : adjacentList){
-			for(Edge edge : verticeNeightbors){
-				int u = edge.getU();
-				int v = edge.getV();
-				graphics.addSegment(u, v, cordinades[u][0],cordinades[u][1], 
-									cordinades[v][0],cordinades[v][1]);
-			}
-		}
-	}
+                cordinades[Integer.parseInt(read[0])][0] = Integer.parseInt(read[1]);
+                cordinades[Integer.parseInt(read[0])][1] = Integer.parseInt(read[2]);
 
-	public Graph(int V) {
-		this.nVertices = V;
-		nEdges = 0;
-		
-		adjacentList = new ArrayList<LinkedList<Edge>>();
-		for(int i=0; i<V; i++){
-			adjacentList.add(new LinkedList<Edge>());
-		}
-	}
+                if (read.length == 3) {
+                    break;
+                }
 
-	public void markNode(int vertice, Color color){
-		this.graphics.setNodeColor(vertice, color);
-	}
+                edges = read[3].split("%");
+                for (String edge : edges) {
+                    e = edge.split(":");
+                    addEdge(Integer.parseInt(read[0]), Integer.parseInt(e[0]),
+                            Double.parseDouble(e[1]));
+                }
+            }
+        } catch (FileNotFoundException ex) {
+            ex.printStackTrace();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        } finally {
+            if (br != null) {
+                try {
+                    br.close();
+                } catch (IOException ex) {
+                    ex.printStackTrace();
+                }
+            }
+        }
+        for (int i = 0; i < nVertices; i++) {
+            graphics.addNode(cordinades[i][0], cordinades[i][1]);
+        }
 
-	public ArrayList<Integer> getOddVertices(){
-		ArrayList<Integer> vertices = new ArrayList<Integer>();
-		if(!isEulerian()){
-			int i = 0;
-			for(LinkedList<Edge> edges : adjacentList){
-				if(edges.size()%2!=0){
-					vertices.add(i);		
-				}
-				i++;
-			}
-		}
-		return vertices;
-	}
+        for (LinkedList<Edge> verticeNeightbors : adjacentList) {
+            for (Edge edge : verticeNeightbors) {
+                int u = edge.getU();
+                int v = edge.getV();
+                graphics.addSegment(u, v, cordinades[u][0], cordinades[u][1],
+                        cordinades[v][0], cordinades[v][1]);
+            }
+        }
+    }
 
-	public void turnEulerian(){
-		ArrayList<Integer> oddVertices = this.getOddVertices();
-		Path path = new Path();		
-		path.setGraph(this);
+    public Graph(int V) {
+        this.nVertices = V;
+        nEdges = 0;
 
-		ArrayList<Edge> createdEdges = new ArrayList<Edge>();
-		int nOddVertices =  oddVertices.size();
-		int nAdded = 0;
-		int[] added = new int[nOddVertices];
-		Arrays.fill(added, 0);		
+        adjacentList = new ArrayList<LinkedList<Edge>>();
+        for (int i = 0; i < V; i++) {
+            adjacentList.add(new LinkedList<Edge>());
+        }
+    }
 
-		for (int i=0; i<nOddVertices; i++) {
-			if(added[i]==1){
-				continue;
-			}			
-			double minDistance = Double.MAX_VALUE;
-			int minV = -1;
+    public void markNode(int vertice, Color color) {
+        this.graphics.setNodeColor(vertice, color);
+    }
 
-			for (int j=0; j<nOddVertices; j++) {
-				if(added[j]==1 || i==j){
-					continue;
-				}
-				double aux = path.findDistance(oddVertices.get(i),oddVertices.get(j));
-				if(aux<minDistance){
-					minDistance = aux;
-					minV = j;
-				}
-			}
+    public ArrayList<Integer> getOddVertices() {
+        ArrayList<Integer> vertices = new ArrayList<Integer>();
+        if (!isEulerian()) {
+            int i = 0;
+            for (LinkedList<Edge> edges : adjacentList) {
+                if (edges.size() % 2 != 0) {
+                    vertices.add(i);
+                }
+                i++;
+            }
+        }
+        return vertices;
+    }
 
-			if(minV!=-1){					
-				added[i] = 1;
-				added[minV] = 1;
-				int u = oddVertices.get(i);
-				int v = oddVertices.get(minV);	
-				addEdge(u, v, minDistance);
-				graphics.addSegment(u, v, cordinades[u][0],cordinades[u][1], 
-										cordinades[v][0],cordinades[v][1]);
-			}
-		}		
-	}
+    public void turnEulerian() {
+        ArrayList<Integer> oddVertices = this.getOddVertices();
+        Path path = new Path();
+        path.setGraph(this);
 
-	public GraphicsUtils getGraphics(){
-		return this.graphics;
-	}
+        ArrayList<Edge> createdEdges = new ArrayList<Edge>();
+        int nOddVertices = oddVertices.size();
+        int nAdded = 0;
+        int[] added = new int[nOddVertices];
+        Arrays.fill(added, 0);
 
-	@Override
-	public String toString(){
-		String str = "";
-		for(LinkedList<Edge> edges : adjacentList){
-			for(Edge e : edges){
-				str += e.toString();
-			}
-			str+="\n";
-		}
-		return str;
-	}
+        for (int i = 0; i < nOddVertices; i++) {
+            if (added[i] == 1) {
+                continue;
+            }
+            double minDistance = Double.MAX_VALUE;
+            int minV = -1;
+
+            for (int j = 0; j < nOddVertices; j++) {
+                if (added[j] == 1 || i == j) {
+                    continue;
+                }
+                double aux = path.findDistance(oddVertices.get(i), oddVertices.get(j));
+                if (aux < minDistance) {
+                    minDistance = aux;
+                    minV = j;
+                }
+            }
+
+            if (minV != -1) {
+                added[i] = 1;
+                added[minV] = 1;
+                int u = oddVertices.get(i);
+                int v = oddVertices.get(minV);
+                addEdge(u, v, minDistance);
+                graphics.addSegment(u, v, cordinades[u][0], cordinades[u][1],
+                        cordinades[v][0], cordinades[v][1]);
+            }
+        }
+    }
+
+    public GraphicsUtils getGraphics() {
+        return this.graphics;
+    }
+
+    @Override
+    public String toString() {
+        String str = "";
+        for (LinkedList<Edge> edges : adjacentList) {
+            for (Edge e : edges) {
+                str += e.toString();
+            }
+            str += "\n";
+        }
+        return str;
+    }
 }
--- src/Hierholzer.java
+++ src/Hierholzer.java
@@ -1,208 +1,210 @@
 
-
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.awt.Color;
 import java.util.Random;
 
 public class Hierholzer {
+
     static private Graph graph;
     private LinkedList<Integer> eulerianCycle;
-    
-    static void slow(){
+
+    static void slow() {
         try {
             Thread.sleep(1000);
-        }catch(InterruptedException e) {
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
-    
-    public static Color getRadomColor(){
+
+    public static Color getRadomColor() {
         Random rand = new Random();
         float r = rand.nextFloat();
         float g = rand.nextFloat();
         float b = rand.nextFloat();
         return new Color(r, g, b);
     }
-    
-    private void InsertCycle(LinkedList<Integer> newCycle, int index){
+
+    private void InsertCycle(LinkedList<Integer> newCycle, int index) {
         this.eulerianCycle.addAll(index, newCycle);
     }
-    
-    private LinkedList<Integer> SearchSimpleCycle(int startVertice){
-    	/*Dani - ta dando errooooo*/
-       LinkedList<Integer> ciclo = new LinkedList<>();
-       ciclo.add(startVertice);
-        
-        if(this.graph.getNeighbours(startVertice) != null){
-            int proxVertice = this.graph.getNeighbours(startVertice).element().getU();
-            this.graph.removeEdge(this.graph.getNeighbours(startVertice).element());
-            SearchSimpleCycle(proxVertice);
-       }
+
+    private LinkedList<Integer> SearchSimpleCycle(int startVertice) {
+        LinkedList<Integer> ciclo = new LinkedList<>();
+        ciclo.add(startVertice);
+        Edge proxAresta;
+        int arestaAtual = startVertice;
+        do {
+            proxAresta = this.graph.getNeighbours(arestaAtual).getFirst();
+            arestaAtual = proxAresta.getV();
+            this.graph.removeEdge(proxAresta);
+            ciclo.add(arestaAtual);
+        } while (arestaAtual != startVertice);
+
         return ciclo;
     }
-    
-    public LinkedList<Integer> SearchEulerianCycle(int startVertice){
+
+    public LinkedList<Integer> SearchEulerianCycle(int startVertice) {
         /*METODO A SER COMPLETADO*/
         return null;
     }
-    
-    /*******************************************************************************************/
-    
-    public static String CycleToString(LinkedList<Integer> cycle){
+
+    /**
+     * ****************************************************************************************
+     */
+    public static String CycleToString(LinkedList<Integer> cycle) {
         String str = "";
         for (Integer integer : cycle) {
-            str += integer +" -> ";
+            str += integer + " -> ";
         }
-        return str.substring(0, str.length()-4);
+        return str.substring(0, str.length() - 4);
     }
-    
-    public void test1(){
+
+    public void test1() {
         Graph graph = new Graph(5);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(0,4,5);
-        graph.addEdge(4,1,5);
-        graph.addEdge(4,2,5);
-        graph.addEdge(4,3,5);
-        
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 5);
+        graph.addEdge(4, 1, 5);
+        graph.addEdge(4, 2, 5);
+        graph.addEdge(4, 3, 5);
+
         System.out.println("Lista de adjacencia do grafo:");
         System.out.println(graph.toString());
     }
-    
-    public void test2(){
+
+    public void test2() {
         Graph graph = new Graph(5);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(0,4,5);
-        graph.addEdge(4,1,5);
-        graph.addEdge(4,2,5);
-        graph.addEdge(4,3,5);
-        
-        
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 5);
+        graph.addEdge(4, 1, 5);
+        graph.addEdge(4, 2, 5);
+        graph.addEdge(4, 3, 5);
+
         System.out.println("Lista de adjacencia do grafo ANTES da remocao:");
         System.out.println(graph.toString());
         System.out.println();
-        
-        graph.removeEdge(new Edge(1,4,5));
-        graph.removeEdge(new Edge(0,4,5));
-        graph.removeEdge(new Edge(0,2,5));
-        
+
+        graph.removeEdge(new Edge(1, 4, 5));
+        graph.removeEdge(new Edge(0, 4, 5));
+        graph.removeEdge(new Edge(0, 2, 5));
+
         System.out.println("Lista de adjacencia do grafo DEPOIS da remocao:");
         System.out.println(graph.toString());
     }
-    
-    public void test3(){
+
+    public void test3() {
         Graph graph = new Graph(5);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(0,4,5);
-        graph.addEdge(4,1,5);
-        graph.addEdge(4,2,5);
-        graph.addEdge(4,3,5);
-        
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 5);
+        graph.addEdge(4, 1, 5);
+        graph.addEdge(4, 2, 5);
+        graph.addEdge(4, 3, 5);
+
         System.out.println("Vertices vizinhos do vertice 0:");
-        for(Edge e : graph.getNeighbours(0)){
-            System.out.print(e.getV()+" ");
+        for (Edge e : graph.getNeighbours(0)) {
+            System.out.print(e.getV() + " ");
         }
         System.out.println();
-        
+
         System.out.println("Vertices vizinhos do vertice 2:");
-        for(Edge e : graph.getNeighbours(2)){
-            System.out.print(e.getV()+" ");
+        for (Edge e : graph.getNeighbours(2)) {
+            System.out.print(e.getV() + " ");
         }
         System.out.println();;
     }
-    
-    public void test4(){
+
+    public void test4() {
         Graph graph = new Graph(5);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(0,4,5);
-        graph.addEdge(4,1,5);
-        graph.addEdge(4,2,5);
-        graph.addEdge(4,3,5);
-        
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 5);
+        graph.addEdge(4, 1, 5);
+        graph.addEdge(4, 2, 5);
+        graph.addEdge(4, 3, 5);
+
         System.out.println("Lista de adjacencia do grafo 1:");
         System.out.print(graph.toString());
         System.out.println("Euleriano:" + graph.isEulerian() + "\n");
-        
+
         graph = new Graph(6);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(1,4,5);
-        graph.addEdge(1,2,5);
-        graph.addEdge(2,3,5);
-        graph.addEdge(2,5,5);
-        graph.addEdge(3,5,5);
-        graph.addEdge(4,5,5);
-        
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(1, 4, 5);
+        graph.addEdge(1, 2, 5);
+        graph.addEdge(2, 3, 5);
+        graph.addEdge(2, 5, 5);
+        graph.addEdge(3, 5, 5);
+        graph.addEdge(4, 5, 5);
+
         System.out.println("Lista de adjacencia do grafo 2:");
         System.out.print(graph.toString());
         System.out.println("Euleriano: " + graph.isEulerian());
     }
-    
-    public void test5(int startVertice){
+
+    public void test5(int startVertice) {
         graph = new Graph(5);
-        graph.addEdge(0,1,5);
-        graph.addEdge(0,2,5);
-        graph.addEdge(0,3,5);
-        graph.addEdge(0,4,5);
-        graph.addEdge(4,1,5);
-        graph.addEdge(4,2,5);
-        graph.addEdge(4,3,5);
-        
-        LinkedList<Integer> cycle =  SearchSimpleCycle(startVertice);
+        graph.addEdge(0, 1, 5);
+        graph.addEdge(0, 2, 5);
+        graph.addEdge(0, 3, 5);
+        graph.addEdge(0, 4, 5);
+        graph.addEdge(4, 1, 5);
+        graph.addEdge(4, 2, 5);
+        graph.addEdge(4, 3, 5);
+
+        LinkedList<Integer> cycle = SearchSimpleCycle(startVertice);
         System.out.println("Ciclo Simples Encontrado: comecando de " + startVertice);
         System.out.println(CycleToString(cycle));
     }
-    
-    public void test6(){
-        Hierholzer hierholzer = new Hierholzer ();
+
+    public void test6() {
+        Hierholzer hierholzer = new Hierholzer();
         //graph = new Graph("grafo1.csv", "white.png", "Roteamento"); // Grafo nao Euleriano
         graph = new Graph("grafo2.csv", "white.png", "Roteamento"); // Grafo Euleriano
-        System.out. println ();
+        System.out.println();
         LinkedList<Integer> eulerianCycle = hierholzer.SearchEulerianCycle(0);
         System.out.println("Cicloâ�£Final: ");
         System.out.println(Hierholzer.CycleToString(eulerianCycle));
         Graph graph2 = new Graph(graph.getGraphics(), "white.png", "Rotaâ�£Final");
-        for(Integer i : eulerianCycle){ slow ();
-            slow ();
-            Color color = Hierholzer.getRadomColor();
-            graph2.markNode(i, color);
-        }
-    }
-    
-    public void test7(){
-        Hierholzer hierholzer = new Hierholzer();
-        // hierholzer.test5(0);
-        
-        // graph = new Graph("grafo.csv", "white.png", "Roteamento");
-        // graph = new Graph("grafo1.csv", "white.png", "Roteamento");
-        // graph = new Graph("grafoMapa1.csv", "mapa1.png", "Roteamento");
-        graph = new Graph("grafoMapa2.csv", "mapa2.png", "Roteamento");
-        
-        
-        System.out.println();
-        LinkedList<Integer> eulerianCycle = hierholzer.SearchEulerianCycle(0);
-        System.out.println("Ciclo Final:");
-        System.out.println(Hierholzer.CycleToString(eulerianCycle));
-        
-        // Graph graph2 = new Graph(graph.getGraphics(), "white.png", "Rota Final");
-        // Graph graph2 = new Graph(graph.getGraphics(), "mapa1.png", "Rota Final");
-        Graph graph2 = new Graph(graph.getGraphics(), "mapa2.png", "Rota Final");
-        for(Integer i: eulerianCycle){
+        for (Integer i : eulerianCycle) {
+            slow();
             slow();
             Color color = Hierholzer.getRadomColor();
             graph2.markNode(i, color);
         }
     }
-    
+
+    public void test7() {
+        Hierholzer hierholzer = new Hierholzer();
+        // hierholzer.test5(0);
+
+        // graph = new Graph("grafo.csv", "white.png", "Roteamento");
+        // graph = new Graph("grafo1.csv", "white.png", "Roteamento");
+        // graph = new Graph("grafoMapa1.csv", "mapa1.png", "Roteamento");
+        graph = new Graph("grafoMapa2.csv", "mapa2.png", "Roteamento");
+
+        System.out.println();
+        LinkedList<Integer> eulerianCycle = hierholzer.SearchEulerianCycle(0);
+        System.out.println("Ciclo Final:");
+        System.out.println(Hierholzer.CycleToString(eulerianCycle));
+
+        // Graph graph2 = new Graph(graph.getGraphics(), "white.png", "Rota Final");
+        // Graph graph2 = new Graph(graph.getGraphics(), "mapa1.png", "Rota Final");
+        Graph graph2 = new Graph(graph.getGraphics(), "mapa2.png", "Rota Final");
+        for (Integer i : eulerianCycle) {
+            slow();
+            Color color = Hierholzer.getRadomColor();
+            graph2.markNode(i, color);
+        }
+    }
+
     public static void main(String[] args) {
         Hierholzer instance = new Hierholzer();
         instance.test5(0);
